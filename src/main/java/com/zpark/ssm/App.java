6c7980e572ab704ecf8fa5739a549d1545be64e6
xhc <xhc@123.cn>
2023-05-06 09:22:04 +0800

    hotfix first commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! xhc second");
+        System.out.println("Hello Git! xhc hotfix first");
     }
 }
