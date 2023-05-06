1feff1298ab9a93c404701af04368a8655e19898
xhc <xhc@123.cn>
2023-05-06 09:17:31 +0800

    second

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,6 +3,6 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-
+        System.out.println("Hello Git! xhc second");
     }
 }
